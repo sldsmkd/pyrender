1983d0779fed25213be2369c157c9408151bc4fb
Kris Holdich <kris@smkd.net>
2019-07-28 11:25:56 +0100

    Chapter 1

--- pyrender/features/steps/tuples_steps.py
+++ pyrender/features/steps/tuples_steps.py
@@ -1,10 +1,13 @@
-from behave import given, then, when, step
+# -- FILE: features/steps/tuples_steps.py
+from behave import given, when, then
 from math import sqrt
-from pyrender.tuples import Tuple, Point, Vector
+from pyrender.tuples import Color, Point, Tuple, Vector
+from pyrender.tuples import cross, dot, is_point, is_vector
+
 
 @given(u'a ← tuple(4.3, -4.2, 3.1, 1.0)')
 def step_impl(context):
-    context.a=Tuple(4.3, -4.2, 3.1, 1.0)
+    context.a = Tuple(4.3, -4.2, 3.1, 1.0)
 
 
 @then(u'a.x = 4.3')
@@ -29,17 +32,17 @@ def step_impl(context):
 
 @then(u'a is a point')
 def step_impl(context):
-    assert context.a.IsPoint() == True
+    assert is_point(context.a) is True
 
 
 @then(u'a is not a vector')
 def step_impl(context):
-    assert context.a.IsVector() == False
+    assert is_vector(context.a) is False
 
 
 @given(u'a ← tuple(4.3, -4.2, 3.1, 0.0)')
 def step_impl(context):
-    context.a=Tuple(4.3, -4.2, 3.1, 0.0)
+    context.a = Tuple(4.3, -4.2, 3.1, 0.0)
 
 
 @then(u'a.w = 0.0')
@@ -49,17 +52,17 @@ def step_impl(context):
 
 @then(u'a is not a point')
 def step_impl(context):
-    assert context.a.IsPoint() == False
+    assert is_point(context.a) is False
 
 
 @then(u'a is a vector')
 def step_impl(context):
-    assert context.a.IsVector() == True
+    assert is_vector(context.a) is True
 
 
 @given(u'p ← point(4, -4, 3)')
 def step_impl(context):
-    context.p=Point(4, -4, 3)
+    context.p = Point(4, -4, 3)
 
 
 @then(u'p = tuple(4, -4, 3, 1)')
@@ -69,7 +72,7 @@ def step_impl(context):
 
 @given(u'v ← vector(4, -4, 3)')
 def step_impl(context):
-    context.v=Vector(4, -4, 3)
+    context.v = Vector(4, -4, 3)
 
 
 @then(u'v = tuple(4, -4, 3, 0)')
@@ -184,12 +187,13 @@ def step_impl(context):
 
 @then(u'magnitude(v) = 1')
 def step_impl(context):
-    assert context.v.Magnitude() == 1
+    assert context.v.magnitude == 1
 
 
 @given(u'v ← vector(0, 1, 0)')
 def step_impl(context):
     context.v = Vector(0, 1, 0)
+
 
 @given(u'v ← vector(0, 0, 1)')
 def step_impl(context):
@@ -203,128 +207,104 @@ def step_impl(context):
 
 @then(u'magnitude(v) = √14')
 def step_impl(context):
-    assert context.v.Magnitude() == sqrt(14)
-
+    assert context.v.magnitude == sqrt(14)
 
 @given(u'v ← vector(-1, -2, -3)')
 def step_impl(context):
     context.v = Vector(-1, -2, -3)
 
-
 @given(u'v ← vector(4, 0, 0)')
 def step_impl(context):
     context.v = Vector(4, 0, 0)
 
-
 @then(u'normalize(v) = vector(1, 0, 0)')
 def step_impl(context):
-    context.v.Normalize() == Vector(1, 0, 0)
-
+    print(context.v.normal)
+    assert context.v.normal == Vector(1, 0, 0)
 
 @then(u'normalize(v) = approximately vector(0.26726, 0.53452, 0.80178)')
 def step_impl(context):
-    context.v.Normalize() == Vector(0.26726, 0.53452, 0.80178)
-
+    assert context.v.normal == Vector(0.26726, 0.53452, 0.80178)
 
 @when(u'norm ← normalize(v)')
 def step_impl(context):
-    context.norm = context.v.Normalize()
-
+    context.norm = context.v.normal
 
 @then(u'magnitude(norm) = 1')
 def step_impl(context):
-    context.norm.Magnitude() == 1
-
+    assert context.norm.magnitude == 1
 
 @given(u'a ← vector(1, 2, 3)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given a ← vector(1, 2, 3)')
-
+    context.a = Vector(1, 2, 3)
 
 @given(u'b ← vector(2, 3, 4)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given b ← vector(2, 3, 4)')
-
+    context.b = Vector(2, 3, 4)
 
 @then(u'dot(a, b) = 20')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then dot(a, b) = 20')
-
+    assert dot(context.a, context.b) == 20
 
 @then(u'cross(a, b) = vector(-1, 2, -1)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then cross(a, b) = vector(-1, 2, -1)')
-
+    assert cross(context.a, context.b) == Vector(-1, 2, -1)
 
 @then(u'cross(b, a) = vector(1, -2, 1)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then cross(b, a) = vector(1, -2, 1)')
-
+    assert cross(context.b, context.a) == Vector(1, -2, 1)
 
 @given(u'c ← color(-0.5, 0.4, 1.7)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given c ← color(-0.5, 0.4, 1.7)')
-
+    context.c = Color(-0.5, 0.4, 1.7)
 
 @then(u'c.red = -0.5')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then c.red = -0.5')
-
+    assert context.c.red == -0.5
 
 @then(u'c.green = 0.4')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then c.green = 0.4')
-
+    assert context.c.green == 0.4
 
 @then(u'c.blue = 1.7')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then c.blue = 1.7')
-
+    assert context.c.blue == 1.7
 
 @given(u'c1 ← color(0.9, 0.6, 0.75)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given c1 ← color(0.9, 0.6, 0.75)')
-
+    context.c1 = Color(0.9, 0.6, 0.75)
 
 @given(u'c2 ← color(0.7, 0.1, 0.25)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given c2 ← color(0.7, 0.1, 0.25)')
-
+    context.c2 = Color(0.7, 0.1, 0.25)
 
 @then(u'c1 + c2 = color(1.6, 0.7, 1.0)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then c1 + c2 = color(1.6, 0.7, 1.0)')
-
+    assert context.c1 + context.c2 == Color(1.6, 0.7, 1.0)
 
 @then(u'c1 - c2 = color(0.2, 0.5, 0.5)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then c1 - c2 = color(0.2, 0.5, 0.5)')
-
+    assert context.c1 - context.c2 == Color(0.2, 0.5, 0.5)
 
 @given(u'c ← color(0.2, 0.3, 0.4)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given c ← color(0.2, 0.3, 0.4)')
-
+    context.c = Color(0.2, 0.3, 0.4)
 
 @then(u'c * 2 = color(0.4, 0.6, 0.8)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then c * 2 = color(0.4, 0.6, 0.8)')
-
+    assert context.c * 2 == Color(0.4, 0.6, 0.8)
 
 @given(u'c1 ← color(1, 0.2, 0.4)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given c1 ← color(1, 0.2, 0.4)')
-
+    context.c1 = Color(1, 0.2, 0.4)
 
 @given(u'c2 ← color(0.9, 1, 0.1)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given c2 ← color(0.9, 1, 0.1)')
-
+    context.c2 = Color(0.9, 1, 0.1)
 
 @then(u'c1 * c2 = color(0.9, 0.2, 0.04)')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then c1 * c2 = color(0.9, 0.2, 0.04)')
-
+    assert context.c1 * context.c2 == Color(0.9, 0.2, 0.04)
 
 @given(u'v ← vector(1, -1, 0)')
 def step_impl(context):
--- pyrender/tuples.py
+++ pyrender/tuples.py
@@ -1,92 +1,138 @@
-import functools
 import numpy as np
-import unittest
 
-class Tuple(object):
-    EPSILON = 0.00001 
 
+class PyrenderTuple():
     def __init__(self, x=0, y=0, z=0, w=0):
-        self._tuple = np.array([x, y, z, w], dtype='float64')
+        self._data = np.array([x, y, z, w], dtype='float64')
+
+    def _construct(self, res):
+        return self.__class__(res[0], res[1], res[2], res[3])
+
+    @property
+    def magnitude(self):
+        return sum(self._data * self._data) ** 0.5
+
+    @property
+    def normal(self):
+        return self._construct(self._data / np.sqrt((self._data ** 2).sum()))
 
     def __str__(self):
-        return str(self._tuple)
+        return str(self._data)
 
-    def __add__(self, other):
-        res = self._tuple + other._tuple
-        return self.__class__(res[0], res[1], res[2], res[3])
-
-    def __sub__(self, other):
-        res = self._tuple - other._tuple
-        return self.__class__(res[0], res[1], res[2], res[3])
-
-    def __mul__(self, scalar):
-        res = self._tuple * scalar
-        return self.__class__(res[0], res[1], res[2], res[3])
-
-    def __truediv__(self, scalar):
-        res = self._tuple / scalar
-        return self.__class__(res[0], res[1], res[2], res[3])
+    def __repr__(self):
+        return self._data
 
     def __eq__(self, other):
-        if np.all(np.abs(self._tuple - other._tuple) < self.EPSILON):
+        EPSILON = 0.00001
+        if np.all(np.abs(self._data - other.__repr__()) < EPSILON):
             return True
         return False
+
+    def __add__(self, other):
+        return self._construct(self._data + other.__repr__())
+
+    def __sub__(self, other):
+        return self._construct(self._data - other.__repr__())
+
+    def __mul__(self, multiplyer):
+        try:
+            return self._construct(self._data * multiplyer)
+        except TypeError:
+            return self._construct(self._data * multiplyer.__repr__())
+
+    def __truediv__(self, divisor):
+        try:
+            return self._construct(self._data / divisor)
+        except TypeError:
+            return self._construct(self._data / divisor.__repr__())
 
     def __neg__(self):
-        res = -self._tuple
-        return self.__class__(res[0], res[1], res[2], res[3])
+        return self._construct(-self._data)
 
-    def Magnitude(self):
-        return np.linalg.norm(self._tuple)
 
-    def Normalize(self):
-        res = self._tuple / self.Magnitude()
-        return self.__class__(res[0], res[1], res[2], res[3])
+class Color(PyrenderTuple):
+    def __init__(self, red=0, green=0, blue=0):
+        self._data = np.array([red, green, blue, 0], dtype='float64')
 
-    def IsPoint(self):
-        if self._tuple[3] == 1:
-            return True
-        return False
+    def _construct(self, res):
+        return self.__class__(res[0], res[1], res[2])
 
-    def IsVector(self):
-        if self._tuple[3] == 0:
-            return True
-        return False
+    @property
+    def red(self):
+        return self._data[0]
+
+    @property
+    def green(self):
+        return self._data[1]
+
+    @property
+    def blue(self):
+        return self._data[2]
+
+
+class Tuple(PyrenderTuple):
+    def __init__(self, x=0, y=0, z=0, w=0):
+        self._data = np.array([x, y, z, w], dtype='float64')
 
     @property
     def x(self):
-        return self._tuple[0]
-
-    @x.setter
-    def x(self, value):
-        self._tuple[0] = value
+        return self._data[0]
 
     @property
     def y(self):
-        return self._tuple[1]
-
-    @y.setter
-    def y(self, value):
-        self._tuple[1] = value
+        return self._data[1]
 
     @property
     def z(self):
-        return self._tuple[2]
-
-    @z.setter
-    def z(self, value):
-        self._tuple[2] = value
+        return self._data[2]
 
     @property
     def w(self):
-        return self._tuple[3]
+        return self._data[3]
 
-    @w.setter
-    def w(self, value):
-        self._tuple[3] = value
 
-def Point(x, y, z):
-    return Tuple(x, y, z, 1)
+class Point(Tuple):
+    def __init__(self, x=0, y=0, z=0):
+        self._data = np.array([x, y, z, 1], dtype='float64')
 
-def Vector(x, y, z):
-    return Tuple(x, y, z, 0)
+    def _construct(self, res):
+        if res[3] == 0:
+            return Vector(res[0], res[1], res[2])
+        elif res[3] == 1:
+            return Point(res[0], res[1], res[2])
+        else:
+            return Tuple(res[0], res[1], res[2], res[3])
+
+
+class Vector(Tuple):
+    def __init__(self, x=0, y=0, z=0):
+        self._data = np.array([x, y, z, 0], dtype='float64')
+
+    def _construct(self, res):
+        if res[3] == 0:
+            return Vector(res[0], res[1], res[2])
+        elif res[3] == 1:
+            return Point(res[0], res[1], res[2])
+        else:
+            return Tuple(res[0], res[1], res[2], res[3])
+
+
+def cross(a, b):
+    res = np.append(np.cross(a.__repr__()[0:3], b.__repr__()[0:3]), [0])
+    return Vector(res[0], res[1], res[2])
+
+
+def dot(a, b):
+    return np.dot(a.__repr__(), b.__repr__())
+
+
+def is_point(a):
+    if a.w == 1.0:
+        return True
+    return False
+
+
+def is_vector(a):
+    if a.w == 0.0:
+        return True
+    return False
